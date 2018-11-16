8dc0faf35092e91099bb6178f8581356f69cd958
aapo haavisto <aapohaavisto@aapos-MacBook-Air.local>
2018-11-16 20:23:22 +0200

    made improvements

--- api/server.js
+++ api/server.js
@@ -1,13 +1,18 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000;
+  port = process.env.PORT || 3000,
+  Task = require('./models/todoListModel'),
+  bodyParser = require('body-parser');
+
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://admin:salasana@database/tododata');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./routes/todoListRoutes');
+routes(app); //register the route
+
 app.listen(port, err => {
   console.log('todo list RESTful API server started on: ' + port);
-});
-
-app.get('/', (req, res) => {
-  res.send('MOI');
 });
